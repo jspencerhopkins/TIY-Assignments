d5c065220750a607632878c181e3f9f840769cfa
J. Spencer Hopkins <jspencerhopkins@gmail.com>
2015-01-07 07:38:27 -0500

    Set up of engToNum -> english words returning integers

---
+++ code/english_to_numbers.rb
@@ -0,0 +1,16 @@
+
+
+
+
+
+def engToNum word
+
+	# need to make sure it is a string in the array
+	if word 
+
+intNum - ''
+
+onesWords = {'one' => 1 , 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9}
+tensWords = {'ten' => 10, 'twenty' => 20, 'thirty' => 30, 'fourty' => 40, 'fifty' => 50, 'sixty' => 60, 'seventy' => 70, 'eighty' => 80, 'ninety' => 90}
+teenagers = {'eleven' => 11, 'twelve' => 12, 'thirteen' => 13, 'fourteen' => 14, 'fifteen' => 15, 'sixteen' => 16, 'seventeen' => 17, 'eighteen' => 18, 'nineteen' => 19}
+
