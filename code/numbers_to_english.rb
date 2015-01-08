81377f6154968df06a796cba806763e81f6f7687
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-08 11:55:28 -0500

    Works through 101

--- code/numbers_to_english.rb
+++ code/numbers_to_english.rb
@@ -6,13 +6,27 @@ class Fixnum
 	def to_english
 		names_as_array = %w(zero one two three four five six seven eight nine
 												ten eleven twelve thirteen fourteen fifteen sixteen
-												seventeen eighteen nineteen twenty
+												seventeen eighteen nineteen
 											)
-		
-		if self > 20
+
+		if self > 99
+			remainder = self % 100
+			if remainder > 0
+				"one hundred #{remainder.to_english}" 
+			else
+				"one hundred"
+			end
+		elsif self > 19
+			
+			tens_names = %w(blank blank twenty thirty fourty fifty sixty seventy eighty ninety)
+
 			tens = self / 10
 			ones = self % 10
-			"twenty #{ones.to_english}"
+			if ones > 0
+				"#{tens_names[tens]} #{ones.to_english}"
+			else 
+				tens_names[tens]
+			end
 		else
 			names_as_array[self]
 		end
@@ -22,6 +36,11 @@ end
 puts 0.to_english #=> "zero"
 puts 1.to_english #=> "one"
 puts 2.to_english #=> "two"
-puts 9.to_english #=> "three"
-puts 21.to_english #=> "three"
-puts 22.to_english #=> "three"
+puts 9.to_english #=> "nine"
+puts 20.to_english #=> "twenty"
+puts 21.to_english #=> "twenty one"
+puts 22.to_english #=> "twenty two"
+puts 30.to_english #=> "thirty"
+puts 99.to_english #=> "ninety nine"
+puts 100.to_english #=> "one hundred"
+puts 101.to_english #=> "one hundred"
