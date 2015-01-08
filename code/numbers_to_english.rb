ee8e5317842d854b3858d7166934ee4ac04957a5
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-08 12:26:19 -0500

    Coverts numbers to words up to trillions

--- code/numbers_to_english.rb
+++ code/numbers_to_english.rb
@@ -9,13 +9,21 @@ class Fixnum
 												seventeen eighteen nineteen
 											)
 
-		if self > 99
-			remainder = self % 100
-			if remainder > 0
-				"one hundred #{remainder.to_english}" 
-			else
-				"one hundred"
-			end
+		if self >= 1_000_000_000_000
+			internal_calculation(1_000_000_000_000, "trillion")
+		
+		elsif self >= 1_000_000_000
+			internal_calculation(1_000_000_000, "billion")
+
+		elsif self >= 1000000
+			internal_calculation(1000000, "million")
+
+		elsif self >= 1000
+			internal_calculation(1000, "thousand")
+	
+		elsif self >= 100
+			internal_calculation(100, "hundred")
+		
 		elsif self > 19
 			
 			tens_names = %w(blank blank twenty thirty fourty fifty sixty seventy eighty ninety)
@@ -31,6 +39,16 @@ class Fixnum
 			names_as_array[self]
 		end
 	end
+
+	def internal_calculation(place, name)		
+			big_number = self / place
+			remainder = self % place
+		if remainder > 0
+			"#{big_number.to_english} #{name} #{remainder.to_english}" 
+		else
+			"#{big_number.to_english} #{name}"
+		end
+	end
 end
 
 puts 0.to_english #=> "zero"
@@ -43,4 +61,10 @@ puts 22.to_english #=> "twenty two"
 puts 30.to_english #=> "thirty"
 puts 99.to_english #=> "ninety nine"
 puts 100.to_english #=> "one hundred"
-puts 101.to_english #=> "one hundred"
+puts 178.to_english #=> "one hundred"
+puts 200.to_english #=> "one hundred"
+puts 543.to_english #=> "one hundred"
+puts 2444.to_english
+puts 999999.to_english
+puts 1000999.to_english
+puts 3_999_888_777.to_english
