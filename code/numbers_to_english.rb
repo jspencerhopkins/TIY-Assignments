6bc86882b2b0cd84c9b2fd31b2b84b6682697712
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-08 11:01:45 -0500

    Create array - works for one through nine

--- code/numbers_to_english.rb
+++ code/numbers_to_english.rb
@@ -1,129 +1,17 @@
+# this is going to be a method that converts
+# integers into their English language representation
 
+class Fixnum
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Fixnum
-
-# 	def number_to_english
-
-# 		# only going to handle positives numbers... for now
-
-# 		if self < 0
-# 			return 'Please enter a number that isn\'t negative.'
-# 		end
-# 		if self == 0
-# 			return 'zero'
-# 		end
-
-# 		numWord = ''
-
-# 		onesPlace = [
-# 			'one',
-# 			'two',
-# 			'three',
-# 			'four',
-# 			'five',
-# 			'six',
-# 			'seven',
-# 			'eight',
-# 			'nine',
-# 		]
-
-# 		tensPlace = [
-# 			'ten',
-# 			'twenty',
-# 			'thirty',
-# 			'fourty',
-# 			'fifty',
-# 			'sixty',
-# 			'seventy',
-# 			'eighty',
-# 			'ninety',
-# 		]
-
-# 		teenagers = [
-# 			'eleven',
-# 			'twelve',
-# 			'thirteen',
-# 			'fourteen',
-# 			'fifteeen',
-# 			'sixteen',
-# 			'seventeen',
-# 			'eighteen',
-# 			'nineteen'
-# 		]
-
-# 		# using 44 as an example
-
-# 		# handling hundreds
-
-# 		left = self
-# 		# now left = 44
-
-# 		write = left/100
-# 		# write = 0.44
-
-# 		left = left - write*100
-# 		# left = 0.44 - (44) = -43.56
-
-# 		if write > 0
-# 			hundreds = write.number_to_english
-# 			numWord = numWord + hundreds - ' hundred'
-
-# 			if left > 0
-# 				numWord = numWord + ' '
-# 			end
-# 		end
-
-# 		# handling tens
+	def to_english
+		names_as_array = %w(zero one two three four five six seven eight nine)
+		names_as_array[self]
 		
 
-# 		write = left/10
-# 		left = left - write*10
+	end
 
-# 		if write > 0
-# 			if ((write ==1) and (left > 0))
-# 				numWord = numWord + teenagers[left-1]
-# 				left = 0
-# 			else
-# 				numWord = numWord + tensPlace[write-1]
-# 			end
+end
 
-# 			if left > 0
-# 				numWord = numWord + ' '
-# 			end
-# 		end
-
-# 		# handling ones
-
-# 		write = left
-# 		left = 0
-
-# 		if write > 0
-# 			numWord = numWord + onesPlace[write -1]
-# 		end
-
-# 		numWord
-
-# 	end	
-# end
-
-
-
-# puts 44.number_to_english
-
+puts 0.to_english #=> "one"
+puts 1.to_english #=> "one"
+puts 2.to_english #=> "two"
