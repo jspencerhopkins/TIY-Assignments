4d154f299cacd34860e94b155bcf33a780a1f6da
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-12 16:02:29 -0500

    Setup Wolf class - define wolf - test wolf

---
+++ code/wolf.rb
@@ -0,0 +1,10 @@
+class Wolf
+
+	def initialize(name, age)
+		@name = name
+		@age = age
+	end
+
+
+
+end
---
+++ code/wolf_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require './wolf'
+
+# A wolfpack is a group of wolves
+# A wolf has a name and an age
+
+class TestWolf < MiniTest::Unit::TestCase
+
+	def setup
+		 @wolf = Wolf.new(:gary, 2)
+  end
+
+  def test_does_wolf_exisit
+  	assert @wolf
+  end
+
+end
