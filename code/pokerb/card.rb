67a5a2efeb90657bf0cfdb58c97ae09f062aa234
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-15 17:15:25 -0500

    Need to re-define cards values for poker

--- code/pokerb/card.rb
+++ code/pokerb/card.rb
@@ -7,15 +7,17 @@ class Card
 		@suit = suit
 	end
 
-	def value
-		if @value.is_a?(Fixnum)
-			@value
-		elsif @value == :ace
-			11
-		else
-			10
-		end
-	end
+	# NEED TO RE-DEFINE CARDS (2..14)
+
+	# def value
+	# 	if @value.is_a?(Fixnum)
+	# 		@value
+	# 	elsif @value == :ace
+	# 		11
+	# 	else
+	# 		10
+	# 	end
+	# end
 	
 	def display
 		"#{@value.to_s.capitalize} of #{suit.to_s.capitalize}"
