c141bd945bdd414e2387ffabf8bb91adb8dd9907
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-16 10:20:52 -0500

    Tweak card.rb to fit rules of poker

--- code/pokerb/card.rb
+++ code/pokerb/card.rb
@@ -9,15 +9,19 @@ class Card
 
 	# NEED TO RE-DEFINE CARDS (2..14)
 
-	# def value
-	# 	if @value.is_a?(Fixnum)
-	# 		@value
-	# 	elsif @value == :ace
-	# 		11
-	# 	else
-	# 		10
-	# 	end
-	# end
+	def value
+		if @value.is_a?(Fixnum)
+			@value
+		elsif @value == :ace
+			14
+		elsif @value == :king
+			13
+		elsif @value == :queen
+			12
+		else 
+			11
+		end
+	end
 	
 	def display
 		"#{@value.to_s.capitalize} of #{suit.to_s.capitalize}"
--- code/pokerb/card_test.rb
+++ code/pokerb/card_test.rb
@@ -2,12 +2,12 @@ require 'minitest/autorun'
 require './card'
 
 # a card has a suit and a value
-# (numbered cards are worth their number, face cards = 10, ace = 11)
+# (numbered cards are worth their number - ace > king > queen, jack > 10
 class CardTest < MiniTest::Unit::TestCase
 
 	def setup
 		@card = Card.new(4, :diamonds)
-		@face_card = Card.new(:king, :spades)
+		@king_card = Card.new(:king, :spades)
 	end
 
 	def test_card_has_value
@@ -18,21 +18,22 @@ class CardTest < MiniTest::Unit::TestCase
 		assert_equal :diamonds, @card.suit
 	end
 
-	def test_face_card_valued_at_10
-		assert_equal 10, @face_card.value
+	def test_king_is_greater_than_queen
+		queen_card = Card.new(:queen, :hearts)
+		assert @king_card.value > queen_card.value
 	end
 
-	def test_ace_valued_at_11
-		ace = Card.new(:ace, :hearts)
-		assert_equal 11, ace.value
-	end
+	# def test_ace_valued_at_11
+	# 	ace = Card.new(:ace, :hearts)
+	# 	assert_equal 11, ace.value
+	# end
 
 	def test_numbered_card_displays_its_suit_and_value
 		assert_equal "4 of Diamonds", @card.display
 	end
 
 	def test_face_card_displays_its_suit_and_value
-		assert_equal "King of Spades", @face_card.display
+		assert_equal "King of Spades", @king_card.display
 	end
 
 
