377ac584b19451f85f36274cef57d82cc4f67743
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-17 10:09:35 -0500

    Test hand has_pair

--- code/pokerb/hand.rb
+++ code/pokerb/hand.rb
@@ -2,10 +2,23 @@ require './card'
 
 class Hand
 
-	attr_reader :cards
+	attr_accessor :cards
 
 	def initialize(cards)
 		@cards = cards
+	end
+
+	def high_card
+		@cards.max_by{|card| card.value}
+	end
+
+	def has_pair?
+		counts = {}
+
+		@cards.each do |card|
+			counts[card.value] = counts[card.value].to_i + 1
+		end
+		counts.any?{|value, number| number == 2 }
 	end
 
 end
