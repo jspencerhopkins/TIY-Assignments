7945e94dca495bd173b53c4aa8c399abe143cabc
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-18 14:20:45 -0500

    Re-define hand class to player class

--- code/pokerb/player.rb
+++ code/pokerb/player.rb
@@ -1,42 +1,42 @@
 require './card'
-class Hand
+class Player
 
 	include Comparable
 
-	attr_accessor :cards
+	attr_accessor :hand
 
-	def initialize(cards)
-		@cards = cards
+	def initialize(cards = [])
+		@hand = cards
 	end
 
 	def high_card
-		@cards.max_by{|card| card.value}
+		@hand.max_by{|card| card.value}
 	end
 
 	def has_pair?
-		all_card_values = @cards.collect{|card| card.value}
+		all_card_values = @hand.collect{|card| card.value}
 		all_card_values.uniq.length == 2		
 	end
 
 	def has_no_matching_cards?
-		all_card_values = @cards.collect{|card| card.value}
+		all_card_values = @hand.collect{|card| card.value}
 		unique_values = all_card_values.uniq
 		all_card_values.length == unique_values.length
 	end
 
 	def has_flush?
-		all_card_suits = @cards.collect{|card| card.suit}
+		all_card_suits = @hand.collect{|card| card.suit}
 		all_card_suits.uniq.length == 1
 	end
 
 	def has_straight?
-		all_card_values = @cards.collect{|card| card.value}
+		all_card_values = @hand.collect{|card| card.value}
 		sorted_values = all_card_values.sort
 		sorted_values[2] - sorted_values[0] == 2
 	end
 
 	def has_3_of_a_kind?
-		all_card_values = @cards.collect{|card| card.value}
+		all_card_values = @hand.collect{|card| card.value}
 		all_card_values.uniq.length == 1		
 	end
 
--- code/pokerb/player_test.rb
+++ code/pokerb/player_test.rb
@@ -3,65 +3,65 @@ require './player'
 require './card'
 
 
-class HandTest < MiniTest::Unit::TestCase
+class PlayerTest < MiniTest::Unit::TestCase
 
 	def setup
 		cards = [Card.new(4, :diamonds), Card.new(:jack, :hearts), Card.new(7, :diamonds)]
-		@hand = Hand.new(cards)
+		@player = Player.new(cards)
 	end		
 
 	def test_hand_has_cards
-		assert @hand.cards
+		assert @player.hand
 	end
 
 	def test_hand_can_have_unique_cards
-		assert @hand.has_no_matching_cards?
+		assert @player.has_no_matching_cards?
 	end
 
 	def test_hand_has_high_card
-		assert_equal 11, @hand.high_card.value
+		assert_equal 11, @player.high_card.value
 	end
 
 	def test_hand_has_a_pair
-		hand_with_pair = Hand.new([Card.new(10, :spades), Card.new(10, :diamonds), Card.new(5, :spades)])
+		hand_with_pair = Player.new([Card.new(10, :spades), Card.new(10, :diamonds), Card.new(5, :spades)])
 		assert hand_with_pair.has_pair?
 	end
 
 	def test_hand_has_3_cards_of_the_same_suit
-		hand_with_flush = Hand.new([Card.new(10, :spades), Card.new(9, :spades), Card.new(5, :spades)])
+		hand_with_flush = Player.new([Card.new(10, :spades), Card.new(9, :spades), Card.new(5, :spades)])
 		assert hand_with_flush.has_flush?
 	end
 
 	def test_hand_has_3_consecutive_cards_of_mixed_suits
-		hand_with_straight = Hand.new([Card.new(:jack, :diamonds), Card.new(:queen, :spades), Card.new(10, :hearts)])
+		hand_with_straight = Player.new([Card.new(:jack, :diamonds), Card.new(:queen, :spades), Card.new(10, :hearts)])
 		assert hand_with_straight.has_straight?
 	end
 
 	def test_hand_has_three_of_a_kind
-		hand_with_three_of_a_kind = Hand.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
+		hand_with_three_of_a_kind = Player.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
 		assert hand_with_three_of_a_kind.has_3_of_a_kind?
 	end
 
 	def test_hand_has_straight_flush
-		hand_with_straight_flush = Hand.new([Card.new(:jack, :diamonds), Card.new(:queen, :diamonds), Card.new(10, :diamonds)])
+		hand_with_straight_flush = Player.new([Card.new(:jack, :diamonds), Card.new(:queen, :diamonds), Card.new(10, :diamonds)])
 		assert hand_with_straight_flush.has_straight_flush?
 	end
 
 	def test_straight_flush_hand_w_Q_J_10_of_H_beats_8_7_6_of_D
-		sf_hand1 = Hand.new([Card.new(:queen, :hearts), Card.new(:jack, :hearts), Card.new(10, :hearts)])
-		sf_hand2 = Hand.new([Card.new(8, :diamonds), Card.new(7, :diamonds), Card.new(6, :diamonds)])
+		sf_hand1 = Player.new([Card.new(:queen, :hearts), Card.new(:jack, :hearts), Card.new(10, :hearts)])
+		sf_hand2 = Player.new([Card.new(8, :diamonds), Card.new(7, :diamonds), Card.new(6, :diamonds)])
 		assert sf_hand1 > sf_hand2
 	end
 
 	def test_straight_flush_hand_beats_3_of_a_kind_hand
-		sf_hand = Hand.new([Card.new(:queen, :hearts), Card.new(:jack, :hearts), Card.new(10, :hearts)])
-		threeKind_hand = Hand.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
+		sf_hand = Player.new([Card.new(:queen, :hearts), Card.new(:jack, :hearts), Card.new(10, :hearts)])
+		threeKind_hand = Player.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
 		assert sf_hand > threeKind_hand
 	end
 
 	def test_3_of_a_kind_hand_beats_straight
-		threeKind_hand = Hand.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
-		straight_hand = Hand.new([Card.new(:jack, :diamonds), Card.new(:queen, :spades), Card.new(10, :hearts)])
+		threeKind_hand = Player.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
+		straight_hand = Player.new([Card.new(:jack, :diamonds), Card.new(:queen, :spades), Card.new(10, :hearts)])
 		assert threeKind_hand > straight_hand
 	end
 end
