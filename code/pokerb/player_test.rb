d961ed0c773af11ed8bf0df9a324ef4d1d17ad04
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-18 14:35:30 -0500

    More player testing

--- code/pokerb/player_test.rb
+++ code/pokerb/player_test.rb
@@ -10,39 +10,39 @@ class PlayerTest < MiniTest::Unit::TestCase
 		@player = Player.new(cards)
 	end		
 
-	def test_hand_has_cards
+	def test_player_has_hand
 		assert @player.hand
 	end
 
-	def test_hand_can_have_unique_cards
+	def test_player_hand_can_have_unique_cards
 		assert @player.has_no_matching_cards?
 	end
 
-	def test_hand_has_high_card
+	def test_player_hand_has_high_card
 		assert_equal 11, @player.high_card.value
 	end
 
-	def test_hand_has_a_pair
+	def test_player_hand_has_a_pair
 		hand_with_pair = Player.new([Card.new(10, :spades), Card.new(10, :diamonds), Card.new(5, :spades)])
 		assert hand_with_pair.has_pair?
 	end
 
-	def test_hand_has_3_cards_of_the_same_suit
+	def test_player_hand_has_3_cards_of_the_same_suit
 		hand_with_flush = Player.new([Card.new(10, :spades), Card.new(9, :spades), Card.new(5, :spades)])
 		assert hand_with_flush.has_flush?
 	end
 
-	def test_hand_has_3_consecutive_cards_of_mixed_suits
+	def test_player_hand_has_3_consecutive_cards_of_mixed_suits
 		hand_with_straight = Player.new([Card.new(:jack, :diamonds), Card.new(:queen, :spades), Card.new(10, :hearts)])
 		assert hand_with_straight.has_straight?
 	end
 
-	def test_hand_has_three_of_a_kind
+	def test_player_hand_has_three_of_a_kind
 		hand_with_three_of_a_kind = Player.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
 		assert hand_with_three_of_a_kind.has_3_of_a_kind?
 	end
 
-	def test_hand_has_straight_flush
+	def test_player_hand_has_straight_flush
 		hand_with_straight_flush = Player.new([Card.new(:jack, :diamonds), Card.new(:queen, :diamonds), Card.new(10, :diamonds)])
 		assert hand_with_straight_flush.has_straight_flush?
 	end
@@ -63,5 +63,10 @@ class PlayerTest < MiniTest::Unit::TestCase
 		threeKind_hand = Player.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
 		straight_hand = Player.new([Card.new(:jack, :diamonds), Card.new(:queen, :spades), Card.new(10, :hearts)])
 		assert threeKind_hand > straight_hand
+	end
+
+	def test_player_hand_has_value
+		hand_with_three_of_a_kind = Player.new([Card.new(:jack, :diamonds), Card.new(:jack, :spades), Card.new(:jack, :hearts)])
+		assert_equal 11_000_000_000_000, hand_with_three_of_a_kind.hand_rank
 	end
 end
