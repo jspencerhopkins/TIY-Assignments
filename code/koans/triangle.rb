7771c47969896c71734b0e2591473e671fb31bf5
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-20 15:38:44 -0500

    Add completed Koans files

---
+++ code/koans/triangle.rb
@@ -0,0 +1,40 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+
+	# Phoned a friend (i.e. the internet for this one).
+	# Was able to grasp the if/else statement to determine which triangle is which.
+	# But very stumped by the TriangleError stuff
+
+	raise TriangleError if [a, b, c].min <= 0
+  first_min, second_min, greater_then_all = [a, b, c].sort
+  raise TriangleError if first_min + second_min <= greater_then_all
+  
+  [:equilateral, :isosceles, :scalene].fetch [a, b, c].uniq.size - 1
+
+	# if a == b && b == c 
+ #    :equilateral
+ #  elsif a == b || a == c || b == c
+ #    :isosceles
+ #  else
+ #    :scalene
+ #  end
+
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
