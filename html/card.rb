ac24f6d27683b9f59f4ae5bd7f251a32af674963
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-21 20:48:33 -0500

    Use Deck class to print all card images

--- html/card.rb
+++ html/card.rb
@@ -1,6 +1,6 @@
 class Card
 
-	attr_reader :suit
+	attr_reader :suit, :value
 
 	def initialize(value, suit)
 		@value = value
@@ -9,19 +9,19 @@ class Card
 
 	# NEED TO RE-DEFINE CARDS (2..14)
 
-	def value
-		if @value.is_a?(Fixnum)
-			@value
-		elsif @value == :ace
-			14
-		elsif @value == :king
-			13
-		elsif @value == :queen
-			12
-		else @value == :jack
-			11
-		end
-	end
+	# def value
+	# 	if @value.is_a?(Fixnum)
+	# 		@value
+	# 	elsif @value == :ace
+	# 		14
+	# 	elsif @value == :king
+	# 		13
+	# 	elsif @value == :queen
+	# 		12
+	# 	else @value == :jack
+	# 		11
+	# 	end
+	# end
 	
 	def display
 		"#{@value.to_s.capitalize} of #{suit.to_s.capitalize}"
