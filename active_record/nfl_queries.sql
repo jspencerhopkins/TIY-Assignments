f7cc9d1bafb0852f8df8088a99c6f87d6b61ac17
jspencerhopkins <jspencerhopkins@gmail.com>
2015-01-27 17:10:42 -0500

    Update nfl_queries sql/rb

--- active_record/nfl_queries.sql
+++ active_record/nfl_queries.sql
@@ -1,26 +1,26 @@
--- Exercise 1
-SELECT * FROM teams WHERE division = 'North' AND conference = 'NFC';
+# Ex 1 -- Select every team from the NFC North
+SELECT teams.name FROM teams WHERE division = 'North' AND conference = 'NFC';
 
--- Exercise 2
-SELECT * FROM teams WHERE wins >= 11;
+# Ex 2 -- Select every team with at least 11 wins
+SELECT teams.name FROM teams WHERE wins >= 11;
 
--- Exercise 3
-SELECT * FROM teams WHERE wins >= 8 AND playoff = 0;
+# Ex 3 -- Select every team with at least 8 wins that did not make the playoffs
+SELECT teams.name FROM teams WHERE wins >= 8 AND playoff = 0;
 
--- Exercise 4
+# Ex 4 -- Rename every team with fewer than 4 wins "Losers"
 UPDATE teams SET name = "Losers" WHERE wins < 4;
 
--- Exercise 5
+# Ex 5 -- Select every team that plays in a city of fewer than 5 million people, and the name of the city
 SELECT cities.name, teams.* FROM cities, teams WHERE teams.city_id = cities.id AND cities.population_2013 < 5000000;
 
--- Exercise 6
+# Ex 6 -- Select every AFC team that plays in a city that is shrinking in population
 SELECT teams.name FROM cities, teams WHERE teams.city_id = cities.id AND cities.population_change < 0 AND teams.conference ='AFC';
 
--- Exercise 7
+# Ex 7 -- Select every city that has a team
 SELECT cities.name FROM cities, teams WHERE teams.city_id = cities.id;
 
--- Exercise 8 
+# Ex 8 -- Select every state that has a team
 SELECT states.name FROM cities, teams, states WHERE teams.city_id = cities.id AND cities.state_id = states.id;
 
--- Exercise 9
+# Ex 9 -- Select every team that plays in California
 SELECT teams.name FROM cities, teams, states WHERE teams.city_id = cities.id AND cities.state_id = states.id AND states.name = 'CALIFORNIA';
